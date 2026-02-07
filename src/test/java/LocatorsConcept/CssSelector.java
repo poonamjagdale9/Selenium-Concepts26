4e567f2702c6f13d991bb4955728f2268cd5361e
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-02-07 22:03:20 +0000

    new changes

--- src/test/java/LocatorsConcept/CssSelector.java
+++ src/test/java/LocatorsConcept/CssSelector.java
@@ -1,13 +1,20 @@
 package LocatorsConcept;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CssSelector {
     public static void main(String args[]){
 
         /*css Cascading Style Sheet
-        // tag id
-        //tag class
-        tag attribute
-                tag class attribute*/
-        
+        // tag id  tag#id
+        //tag class  tag.classname
+        tag attribute tag[attribute ="value"]
+                tag class attribute tag.classname[attribute="value"]*/
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https:demo.nopcommerce.com");
+        driver.manage().window().maximize();
+
     }
 }
