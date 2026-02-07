42c9f310a06e0cdf2f4b46a22c344f8e11ed9f15
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-02-07 21:55:51 +0000

    new changes

---
+++ src/test/java/LocatorsConcept/CssSelector.java
@@ -0,0 +1,13 @@
+package LocatorsConcept;
+
+public class CssSelector {
+    public static void main(String args[]){
+
+        /*css Cascading Style Sheet
+        // tag id
+        //tag class
+        tag attribute
+                tag class attribute*/
+        
+    }
+}
