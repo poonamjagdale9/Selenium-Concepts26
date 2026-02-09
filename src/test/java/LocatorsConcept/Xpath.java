1e0f3815372963f1407675ed7b60e12f3010292f
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-02-09 14:12:51 +0000

    new changes

---
+++ src/test/java/LocatorsConcept/Xpath.java
@@ -0,0 +1,14 @@
+package LocatorsConcept;
+
+public class Xpath {
+    public static void main(String args[]){
+        /* 2 types of Xpath
+        // Absolute xpath (full xpath) always starts from root node
+        Ex: /html/body/div[6]/header/div[2]/div[2]/form/input
+
+        // Relative xpath(parcial xpath) we use atributes- it will directly jump to the element
+        EX: //*[@name="search"]   //input[@id='small-searchterms']
+        //Xpath is address of the element on the DOM */
+
+    }
+}
