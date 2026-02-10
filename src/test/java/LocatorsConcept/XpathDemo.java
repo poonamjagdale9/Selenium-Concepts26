6ea5d461c11643b3b4944a025ff00c7c85245958
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-02-10 09:58:15 +0000

    new changes

--- src/test/java/LocatorsConcept/XpathDemo.java
+++ src/test/java/LocatorsConcept/XpathDemo.java
@@ -1,7 +1,17 @@
 package LocatorsConcept;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class XpathDemo {
     public static void main(String args[]){
-        
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+
+        // Xpath with the single attribute-- for search box
+        driver.findElement(By.xpath("//*[@id=\"small-searchterms\"]")).sendKeys("Mac");
+
     }
 }
