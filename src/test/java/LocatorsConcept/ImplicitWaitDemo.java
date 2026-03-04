661814d75d1ce2d3111a1f6467df28ebbc127510
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-03-04 11:38:34 +0000

    new Changes

--- src/test/java/LocatorsConcept/ImplicitWaitDemo.java
+++ src/test/java/LocatorsConcept/ImplicitWaitDemo.java
@@ -13,11 +13,17 @@ public class ImplicitWaitDemo {
         /*always write implicit wait after creating the driver instance so we do not
         need to write wait for every element where synchronisation issue happens
         it will aplly for every statement of the code if we have 100 line of code it will apply
-        for every line of the code
-        * */
+        for every line of the code, it will be active until we close our driver/browser
+        * Implicit wait will not west the time like Thread .sleep if element found in 2se
+        rest
+        Disadvantages - Implicit wait will wait only for given time, if element does not found in that time it
+        will throw NoSuchElement exception.
+        */
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().window().maximize();
         driver.findElement(By.xpath("//input[@placeholder=\"Username\"]")).sendKeys("Admin");
+
+        //driver.close();
 
     }
 }
