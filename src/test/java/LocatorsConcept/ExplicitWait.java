906478f9a2cc6c9e78942c478a50150f5e87fa9c
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-03-05 22:06:21 +0000

    new Changes

--- src/test/java/LocatorsConcept/ExplicitWait.java
+++ src/test/java/LocatorsConcept/ExplicitWait.java
@@ -3,16 +3,23 @@ package LocatorsConcept;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ExplicitWait {
     public static void main(String args[]){
         /*
         * Declaration
-        * Usage*/
+        * Usage
+        * we need to use webdriver wait class*/
         WebDriver driver = new ChromeDriver();
+
+        WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(5)); // declaration
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login\"");
         driver.manage().window().maximize();
         driver.findElement(By.xpath("//input[@placeholder=\"Username\"]")).sendKeys("Admin");
         driver.close();
+        
     }
 }
