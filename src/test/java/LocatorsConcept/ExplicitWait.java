7f5e70157b04d781bbc8933627b32a0d0d08d39f
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-03-06 11:18:29 +0000

    new Changes

--- src/test/java/LocatorsConcept/ExplicitWait.java
+++ src/test/java/LocatorsConcept/ExplicitWait.java
@@ -2,6 +2,7 @@ package LocatorsConcept;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,14 +15,21 @@ public class ExplicitWait {
         /*
         * Declaration
         * Usage
-        * we need to use webdriver wait class*/
+        * we need to use webdriver wait class
+        * finding WebElement is inclusive in Explicit wait*/
         WebDriver driver = new ChromeDriver();
         WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(5)); // declaration
-        mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder=\"Username\"]")));
+
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login\"");
         driver.manage().window().maximize();
+
+        WebElement txtUsername=
+                mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder=\"Username\"]")));
+        //when we use Explicitwait it also returns a webelement directly so we can perform actions
+
+        txtUsername.sendKeys("Admin");
      //   driver.findElement(By.xpath("//input[@placeholder=\"Username\"]")).sendKeys("Admin");
-        driver.close();
+       // driver.close();
 
     }
 }
