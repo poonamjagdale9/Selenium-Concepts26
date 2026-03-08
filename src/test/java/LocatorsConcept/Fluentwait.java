7cb8d9ada1e18b4058d13b094ab80988c6ac78a8
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-03-08 22:35:27 +0000

    new Changes

--- src/test/java/LocatorsConcept/Fluentwait.java
+++ src/test/java/LocatorsConcept/Fluentwait.java
@@ -1,10 +1,13 @@
 package LocatorsConcept;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
 
 public class Fluentwait {
 
@@ -12,7 +15,10 @@ public class Fluentwait {
         WebDriver driver = new ChromeDriver();
 
         // Fluent wait declaration
-        Wait<WebDriver> mywait= new FluentWait<WebDriver>(driver);
+        Wait<WebDriver> mywait= new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                        .pollingEvery(Duration.ofSeconds(5))
+                                .ignoring(NoSuchElementException.class);
 
         driver.get("\"https://opensource-demo.orangehrmlive.com/web/index.php/auth/login\\\"");
         driver.manage().window().maximize();
