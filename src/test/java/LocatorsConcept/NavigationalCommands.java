440f4ed28e795e85a3564895799101cce1dc6fc6
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-03-09 13:58:41 +0000

    new Changes

--- src/test/java/LocatorsConcept/NavigationalCommands.java
+++ src/test/java/LocatorsConcept/NavigationalCommands.java
@@ -1,5 +1,8 @@
 package LocatorsConcept;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class NavigationalCommands {
     public static void main(String args[]){
        /* Navigational commands
@@ -7,6 +10,16 @@ public class NavigationalCommands {
        * navigate().back()
        * navigate.forward()
        * navigate().refresh()
+       * //Difference between two methods
+       * Navigate.to() method internally call get() only
+       * get method uses String parameter only, where navigate().to()mentod uses Strin parameter as well as
+       * URL objects as well
        * */
+
+        WebDriver driver = new ChromeDriver();
+       // driver.get("https://demo.nopcommerce.com/");
+        // same we can achieve by using navigate command
+
+        driver.navigate().to("https://demo.nopcommerce.com/");
     }
 }
