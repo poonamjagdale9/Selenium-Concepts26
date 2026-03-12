1739b22f29a8d631f6598282a22f61f721ddc240
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-03-12 13:33:46 +0000

    new Changes

---
+++ src/test/java/LocatorsConcept/ClosingSpecificbrowserWindow.java
@@ -0,0 +1,21 @@
+package LocatorsConcept;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.Set;
+
+public class ClosingSpecificbrowserWindow {
+    public static void main(String args[]){
+        WebDriver driver= new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(500));
+
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.linkText("Facebook")).click();
+        
+        Set<String> windowIds = driver.getWindowHandles();
+    }
+}
