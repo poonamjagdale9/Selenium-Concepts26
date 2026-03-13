dd934e8712ff0b13fa1fee6b3a97943aa511dba9
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-03-13 23:05:18 +0000

    new Changes

--- src/test/java/Checkboxes/HandleCheckBoxes.java
+++ src/test/java/Checkboxes/HandleCheckBoxes.java
@@ -20,12 +20,12 @@ public class HandleCheckBoxes {
        System.out.println(status1);
 
        //select specific check box
-      driver.findElement(By.xpath("//input[@id='checkbox1']")).click();
+     /* driver.findElement(By.xpath("//input[@id='checkbox1']")).click();
         boolean status2= driver.findElement(By.id("checkbox1")).isSelected();
-        System.out.println(status2);
+        System.out.println(status2);*/
 
         //select multiple check boxes
-       List<WebElement> checkboxes= driver.findElements(By.xpath("//input[@class=form-check-input]"));
+       List<WebElement> checkboxes= driver.findElements(By.xpath("//input[@class=form-check-input and @type=\"checkbox\"]"));
         for (int i=0; i<=checkboxes.size();i++){
             checkboxes.get(i).click();
         }
