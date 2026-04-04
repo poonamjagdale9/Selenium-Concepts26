5529ef3f0b300debdb161321c4394e75ebd5875d
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-04-04 21:21:42 +0100

    new changes

--- src/test/java/NewTestCase/Test1.java
+++ src/test/java/NewTestCase/Test1.java
@@ -7,7 +7,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test1 {
 
 public static void main(String args[]){
-    
+    WebDriver driver = new ChromeDriver();
+
+    driver.get("https:www.google.com");
+   
 }
     }
 
