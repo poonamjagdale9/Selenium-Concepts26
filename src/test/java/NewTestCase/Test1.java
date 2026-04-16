ad05bb7500a0029890ef884d9192f2302a6db74f
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-04-16 18:53:13 +0100

    new changes

--- src/test/java/NewTestCase/Test1.java
+++ src/test/java/NewTestCase/Test1.java
@@ -6,11 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test1 {
     public static void main(String args[]){
-        WebDriver driver = new ChromeDriver();
-        driver.get("https:www.google.com");
-        driver.findElement(By.id("L2AGLb")).click();
-        driver.findElement(By.id("APjFqb")).sendKeys("Argoes");
-
+       
     }
 
 
