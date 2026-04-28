24b77782a04c8952bfba59984081f8ded56b0ea2
Poonam Jagdale <poonamsjagdale9@gmail.com>
2026-04-28 19:52:03 +0100

    new changes

--- src/test/java/NewTestCase/LoginTest.java
+++ src/test/java/NewTestCase/LoginTest.java
@@ -11,25 +11,26 @@ import java.time.Duration;
 
 public class LoginTest {
     WebDriver driver;
+    LoginPage lp = new LoginPage(driver);
+
     @BeforeClass
-      void setup(){
-        driver= new ChromeDriver();
+    void setup() {
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().window().maximize();
     }
 
     @Test
-    void testLogin(){
-    LoginPage lp =new LoginPage(driver);
-    lp.setPassword("admin");
-    lp.setPassword("admin123");
-    lp.clickLogin();
-        Assert.assertEquals(driver.getTitle(),"OrangeHRM");
+    void testLogin() {
+        lp.setPassword("admin");
+        lp.setPassword("admin123");
+        lp.clickLogin();
+        Assert.assertEquals(driver.getTitle(), "OrangeHRM");
     }
 
-    @AfterClass
     void tearDown(){
-    driver.close();
+        driver.close();
     }
+
 }
